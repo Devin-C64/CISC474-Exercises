e2d6cae18a426f041b9fb68a9e0873272ae1c244
Devin <devinjc@udel.edu>
2023-11-28 16:17:51 -0500

    secret key fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./etc/secrets/exercise5key.json");
+var serviceAccount = require("/etc/secrets/exercise5key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
