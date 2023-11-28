c67e78c5666f1e318bf7d4e2cc519a3b415d8c46
Devin <devinjc@udel.edu>
2023-11-28 08:35:47 -0500

    Yarn fix

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Get a database reference to our blog
+const { getDatabase } = require('firebase-admin/database');
+const db = getDatabase();
+const ref = db.ref('server/saving-data/');
+const usersRef = ref.child('users');
+usersRef.set({
+alanisawesome: {
+date_of_birth: 'June 23, 1912',
+full_name: 'Alan Turing'
+},
+gracehop: {
+date_of_birth: 'December 9, 1906',
+full_name: 'Grace Hopper'
+}
+});
+
+
 var admin = require("firebase-admin");
 
 var serviceAccount = require("path/to/serviceAccountKey.json");
