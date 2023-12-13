bb15c3f2b6b9897f076e2a17f273caf737448abd
Devin <devinjc@udel.edu>
2023-12-13 12:49:02 -0500

    Started exercise 4

--- index.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
-  $('#go').click(function(){ 
-    var fname = $('#fname').val();
-    var lname = $('#lname').val();
-    $('#jumbotron').text(fname + " " + lname);
-  })
-});
